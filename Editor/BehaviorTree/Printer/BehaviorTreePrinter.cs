868ce212d6006bd2b3f3fa9fc942e5a1850206ff
Ash Blue <ash@blueashes.com>
2019-06-06 09:57:34 -0600

    fix(behavior-tree): recursive global position changes don't double up

---
+++ Editor/BehaviorTree/Printer/BehaviorTreePrinter.cs
@@ -0,0 +1,16 @@
+namespace CleverCrow.Fluid.BTs.Trees.Editors {
+    public class BehaviorTreePrinter {
+        private readonly VisualTask _root;
+        
+        public BehaviorTreePrinter (IBehaviorTree tree) {
+            var container = new GraphContainerVertical();
+            // @TODO Temporary start position for debugging, replace with a proper scroll view
+            container.SetGlobalPosition(300, 0);
+            _root = new VisualTask(tree.Root, container);
+        }
+
+        public void Print () {
+            _root.Print();
+        }
+    }
+}
---
+++ Editor/BehaviorTree/Printer/VisualTask.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using CleverCrow.Fluid.BTs.Tasks;
+using UnityEngine;
+
+namespace CleverCrow.Fluid.BTs.Trees.Editors {
+    public class VisualTask {
+        private float _width = 50;
+        private float _height = 50;
+        
+        private List<VisualTask> _children = new List<VisualTask>();
+        private IGraphBox _box;
+        private ITask _task;
+
+        public VisualTask (ITask task, IGraphContainer parentContainer) {
+            _task = task;
+            
+            var container = new GraphContainerVertical();
+
+            _box = new GraphBox();
+            _box.SetSize(50, 50);
+            
+            container.AddBox(_box);
+
+            if (task.Children != null) {
+                var childContainer = new GraphContainerHorizontal();
+                foreach (var child in task.Children) {
+                    _children.Add(new VisualTask(child, childContainer));
+                }
+                container.AddBox(childContainer);
+            }
+
+            parentContainer.AddBox(container);
+        }
+
+        public void Print () {
+            var rect = new Rect(_box.GlobalPositionX, _box.GlobalPositionY, _box.Width, _box.Height);
+            GUI.Box(rect, _task.Name);
+            
+            foreach (var child in _children) {
+                child.Print();
+            }
+        }
+    }
+}
